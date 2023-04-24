ccac0c4016f2df15f4aec832b1cf7477cc22ffb3
Gleb Tolstenev <yonnarge@student.21-school.ru>
2023-04-25 00:08:16 +0300

    Add solution for task 2, remove CMake

--- 1.c
+++ 1.c
@@ -2,52 +2,112 @@
 #include <stdlib.h>
 #define K 32
 
-/* Организовать безопасный ввод с клавиатуры двух вещественных чисел.
- * Вывести на экран в порядке убывания целые числа, расположенные
- * на координатной оси между двумя введенными числами.*/
-
-float
-
-int main(void) {
-//    float first = 5.6, second = 1.3;
-    float first = 0, second = 0;
+float get_float() {
+    float b;
     char buff[K];
-    char buff2[K];
     char *end;
 
-    do {
-        printf("Enter the first number: ");
-        fgets(buff, sizeof(buff), stdin);
-        first = strtof(buff, &end);
-
-        if(!first) {
-            printf("Incorrect input, please try again\n");
+    while (1) {
+        fgets (buff,sizeof(buff),stdin);
+        b = strtof(buff, &end);
+        if(buff == end || *end != '\n') {
+            printf("Incorrect input, please try again: ");
             continue;
         }
+        break;
+    }
+    return b;
+}
 
-        printf("Enter the second number: ");
-        fgets(buff2, sizeof(buff2), stdin);
-        second = strtof(buff2, &end);
+int main(void) {
+    float first = 0, second = 0, tmp;
 
-        if(!second) {
-            printf("Incorrect input, please try again\n");
-            continue;
-        }
+    printf("Enter the first number: ");
+    first = get_float();
+    printf("Enter the second number: ");
+    second = get_float();
 
-    } while(first == 0 || second == 0);
+    if (first > second) {
+        tmp = second;
+        second = first;
+        first = tmp;
+    }
 
     int min = (int)first;
     int max = (int)second;
-    int tmp;
 
-    if (min > max) {
-        tmp = min;
-        min = max;
-        max = tmp;
-    }
+    if ((float)max == second) max--;
+
     while (max > min) {
         printf("%d ", max--);
     }
     printf("\n");
     return (0);
 }
+
+//./test
+//Enter the first number: 33
+//Enter the second number: 31
+//max: 32
+//min: 31
+//32
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#define K 32
+//
+//float get_float() {
+//    float b;
+//    char buff[K];
+//    char *end;
+//
+//    while (1) {
+//        fgets(buff, sizeof(buff), stdin);
+//        b = strtof(buff, &end);
+//        if (buff == end || *end != '\n') {
+//            printf("Incorrect input, please try again: ");
+//            continue;
+//        }
+//        break;
+//    }
+//    return b;
+//}
+//
+//int main(){
+//    float first;
+//    float second;
+//    printf("Vvedite pervoe chislo: ");
+//    a = get_float();
+//    printf("Vvedite vtoroe chislo: ");
+//    b = get_float();
+//
+//    int min = (int)a;
+//    int max = (int)b;
+//    int k;
+//
+//    printf("max: %d\n", max);
+//    printf("min: %d\n", min);
+//
+//
+//    if ((float)min == a) min--;
+//    if ((float)max == b) max--;
+//
+//    if (max < min) {
+//        k = max;
+//        max = min;
+//        min = k;
+//    }
+//
+//    printf("max: %d\n", max);
+//    printf("min: %d\n", min);
+//
+//    while (max > min)
+//        printf("%d ", max--);
+//    printf("\n");
+//    return 0;
+//}
+//
+
+
+
+
---
+++ 2.c
@@ -0,0 +1,88 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define Z 32
+
+float get_float(void) {
+    float b;
+    char buff[Z];
+    char *end;
+
+    while (1) {
+        fgets(buff, sizeof(buff), stdin);
+        b = strtof(buff, &end);
+        if (buff == end || *end != '\n') {
+            puts("Incorrect input. Please, try again");
+            continue;
+        }
+        break;
+    }
+    return b;
+}
+
+int get_int(void) {
+    int a, amo;
+    long long b;
+    char buff[Z];
+    char *end;
+
+    while (1) {
+        fgets(buff, sizeof(buff), stdin);
+        amo = sscanf(buff, "%d", &a);
+        if (1 != amo) {
+            puts("Incorrect input. Please, try again");
+            continue;
+        }
+
+        b = strtol(buff, &end, 10);
+        if ((buff != end) && (*end != '\n')) {
+            puts("Incorrect input. Please, try again");
+            continue;
+        }
+
+        if (a != b) {
+            puts("Incorrect input. Please, try again");
+            continue;
+        }
+
+        break;
+    }
+    return b;
+}
+
+double my_pow(float base, int exp) {
+    double n = base;
+
+    for(int i = 0; i < exp-1; i++) n *= base;
+
+    return n;
+}
+
+int main(void) {
+    int a, b, c, d;
+    float e, f, g;
+    double x;
+
+    puts("Enter the 'a' value:");
+    a = get_int();
+    puts("Enter the 'b' value:");
+    b = get_int();
+    puts("Enter the 'c' value:");
+    c = get_int();
+    puts("Enter the 'd' value:");
+    d = get_int();
+    puts("Enter the 'e' value:");
+    e = get_float();
+    puts("Enter the 'f' value:");
+    f = get_float();
+    puts("Enter the 'g' value:");
+    g = get_float();
+
+    x = my_pow(e,a) * (my_pow(g,b) - my_pow(f, c)) + ((g - e) / my_pow(f,d));
+    double orig = pow(e,a) * (pow(g,b) - pow(f, c)) + ((g - e) / pow(f,d));
+    printf("x: %lf\n", x);
+    printf("o: %lf\n", orig);
+    return 0;
+}
+
