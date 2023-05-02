74c2cc368a996c83ff05e2534f32bd31ed5fb30a
Gleb Tolstenev <yonnarge@student.21-school.ru>
2023-05-02 19:16:30 +0300

    Decompose into files header.h main.c functions.c

---
+++ task_02.05.2023/header.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 3
+#define Z 32
+
+float my_abs(float a);
+
+void my_swap(int *a, int *b);
+
+int get_int(void);
+
+void print_matrix(int m[N][N]);
--- task_02.05.2023/main.c
+++ task_02.05.2023/main.c
@@ -9,68 +9,7 @@
 4. Разбить программу на несколько исходных файлов.
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#define N 3
-#define Z 32
-
-float my_abs(float a);
-
-void my_swap(int *a, int *b);
-
-int get_int(void);
-
-void print_matrix(int m[N][N]);
-
-void print_matrix(int m[N][N]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%d\t", m[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-float my_abs(float a) {
-    if (a < 0) {
-        a = -a;
-    }
-    return a;
-}
-
-void my_swap(int *a, int *b) {
-    int buff = *a;
-    *a = *b;
-    *b = buff;
-}
-
-int get_int(void) {
-    int a, amo;
-    long long b;
-    char buff[Z];
-    char *end;
-
-    while (1) {
-        fgets(buff, sizeof(buff), stdin);
-        amo = sscanf(buff, "%d", &a);
-        if (1 != amo) {
-            puts("Incorrect input. Please, try again:");
-            continue;
-        }
-        b = strtol(buff, &end, 10);
-        if ((buff != end) && (*end != '\n')) {
-            puts("Incorrect input. Please, try again:");
-            continue;
-        }
-        if (a != b) {
-            puts("Incorrect input. Please, try again:");
-            continue;
-        }
-        break;
-    }
-    return b;
-}
+#include "header.h"
 
 int main(void) {
     int m[N][N];
