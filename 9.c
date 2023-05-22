496785d6c0a743b26eb94643d04a3868e5ed1b19
Gleb Tolstenev <tolstenev.gleb@mail.ru>
2023-05-22 18:19:16 +0300

    Update 9.c

--- 9.c
+++ 9.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define M 4
-#define N 3
+#define N 4
+#define K 3
 #define Z 32
 
 int get_int(void) {
@@ -32,40 +32,60 @@ int get_int(void) {
 }
 
 int main() {
-    int m[M][N];
+    int m[N][K];
 
     printf("Enter the matrix elements:\n");
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < K; j++) {
             printf("Enter the matrix (%d, %d) element: ", i, j);
             m[i][j] = get_int();
         }
     }
 
     printf("Matrix:\n");
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < K; j++) {
             printf("%d ", m[i][j]);
         }
         printf("\n");
     }
-    
+
+    // variant 1
     printf("Peripheral elements: ");
-    for (int j = 0; j < N; j++) {
+    if (N == 1 && K == 1) {
+        printf("%d ", m[0][0]);
+    }
+    for(int j = 0; j < K - 1; j++){
         printf("%d ", m[0][j]);
     }
-    for (int i = 1; i < M; i++) {
-        printf("%d ", m[i][N-1]);
+    for(int i = 0; i < N - 1; i++){
+        printf("%d ", m[i][K-1]);
     }
-    if (N > 1 && M > 1) {
-        for (int j = N - 2; j >= 0; j--) {
-            printf("%d ", m[M - 1][j]);
-        }
-        for (int i = M - 2; i >= 1; i--) {
-            printf("%d ", m[i][0]);
-        }
+    for(int j = K - 1; j > 0; j--){
+        printf("%d ", m[N-1][j]);
+    }
+    for(int i = N - 1; i > 0; i--){
+        printf("%d ", m[i][0]);
     }
     printf("\n");
+
+//    variant 2
+//    printf("Peripheral elements: ");
+//    for (int j = 0; j < K; j++) {
+//        printf("%d ", m[0][j]);
+//    }
+//    for (int i = 1; i < N; i++) {
+//        printf("%d ", m[i][K-1]);
+//    }
+//    if (K > 1 && N > 1) {
+//        for (int j = K - 2; j >= 0; j--) {
+//            printf("%d ", m[N - 1][j]);
+//        }
+//        for (int i = N - 2; i >= 1; i--) {
+//            printf("%d ", m[i][0]);
+//        }
+//    }
+//    printf("\n");
 
     return 0;
 }
